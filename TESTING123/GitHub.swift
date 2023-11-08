32fa96da3fae4fe3a2bad80597d336f81ad5b38d
jrobins3645 <jeff.h.robinson@gmail.com>
2023-11-08 14:21:23 -0500

    Message: "banana"

---
+++ TESTING123/GitHub.swift
@@ -0,0 +1,18 @@
+//
+//  GitHub.swift
+//  TESTING123
+//
+//  Created by Jeff on 11/8/23.
+//
+
+import SwiftUI
+
+struct GitHub: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitHub()
+}

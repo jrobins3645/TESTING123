ed2e0b8016fa5f88b7085a12af1b5bb72e44c784
jrobins3645 <jeff.h.robinson@gmail.com>
2023-11-08 14:19:38 -0500

    Initial Commit

---
+++ TESTING123/TESTING123App.swift
@@ -0,0 +1,17 @@
+//
+//  TESTING123App.swift
+//  TESTING123
+//
+//  Created by Jeff on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TESTING123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
